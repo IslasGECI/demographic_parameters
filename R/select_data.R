babfc763ea140d5a9f0099af687e4d13e97b4128
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-08 11:50:21 -0700

    ♻️  Refactor

--- R/select_data.R
+++ R/select_data.R
@@ -1,3 +1,3 @@
 select_data <- function(raw_data) {
-  return(raw_data |> dplyr::select(c("Fecha", "Hora", "Isla", "Especie", "Nombre_o_ID_del_sitio_o_colonia", "ID_del_nido_o_madriguera", "ID_del_anillo", "Cantidad_nidos", "Cantidad_huevos", "Cantidad_pollos_volanton")))
+  raw_data |> dplyr::select(c("Fecha", "Hora", "Isla", "Especie", "Nombre_o_ID_del_sitio_o_colonia", "ID_del_nido_o_madriguera", "ID_del_anillo", "Cantidad_nidos", "Cantidad_huevos", "Cantidad_pollos_volanton"))
 }
