ab1e1ad37845d95510b3304972a728c26e534e0f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-08 11:49:26 -0700

    ♻️  Refactor

--- tests/testthat/test_select_data.R
+++ tests/testthat/test_select_data.R
@@ -3,7 +3,7 @@ describe("Select data", {
   it("select columns", {
     obtained <- select_data(raw_data)
     obtained_columns <- colnames(obtained)
-    expected_columns <- c("Fecha", "Hora", "Isla", "Especie", "Nombre_o_ID_del_sitio_o_colonia", "ID_del_nido_o_madriguera", "ID_del_anillo,Cantidad_nidos", "Cantidad_huevos", "Cantidad_pollos_volanton")
+    expected_columns <- c("Fecha", "Hora", "Isla", "Especie", "Nombre_o_ID_del_sitio_o_colonia", "ID_del_nido_o_madriguera", "ID_del_anillo", "Cantidad_nidos", "Cantidad_huevos", "Cantidad_pollos_volanton")
     expect_equal(obtained_columns, expected_columns)
   })
 })
