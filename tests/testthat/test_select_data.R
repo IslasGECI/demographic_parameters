0892ee5b26277c2c82af0a9a2d7e872b9b39bfd1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-08 12:03:18 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_select_data.R
+++ tests/testthat/test_select_data.R
@@ -1,9 +1,16 @@
 describe("Select data", {
   raw_data <- readr::read_csv("/workdir/tests/data/censo_aves_superficie_todas_islas_2018.csv", show_col_types = FALSE)
+  obtained <- select_data(raw_data)
   it("select columns", {
-    obtained <- select_data(raw_data)
     obtained_columns <- colnames(obtained)
     expected_columns <- c("Fecha", "Hora", "Isla", "Especie", "Nombre_o_ID_del_sitio_o_colonia", "ID_del_nido_o_madriguera", "ID_del_anillo", "Cantidad_nidos", "Cantidad_huevos", "Cantidad_pollos_volanton")
     expect_equal(obtained_columns, expected_columns)
+  })
+  it("clean data for hatching success", {
+    print(obtained$Cantidad_huevos)
+    print(!is.na(obtained$Cantidad_huevos))
+    is_valid_row <- all(!is.na(obtained$Cantidad_huevos))
+    print(is_valid_row)
+    expect_true(is_valid_row)
   })
 })
