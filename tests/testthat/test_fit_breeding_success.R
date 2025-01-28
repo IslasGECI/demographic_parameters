2147f16c62b465384870346505562c5c5e0ba0f6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-28 14:38:11 -0800

    🛑🧪 Fail tests

--- tests/testthat/test_fit_breeding_success.R
+++ tests/testthat/test_fit_breeding_success.R
@@ -1,10 +1,18 @@
 describe("`fit_breeding_success()`", {
   long_egg_data <- tibble::tibble(
     Temporada = c(2016, 2016, 2017, 2017, 2018, 2018, 2018),
-    breeding_success = c(1, 1, 0, 0, 0.33, 0.33, 0.33)
+    is_chick_fledged = c(1, 0, 0, 0, 1, 1, 1)
   )
   it("fit breeding success using binomial family", {
     model <- fit_breeding_success(data = long_egg_data)
     coef_values <- coef(model)
+    print(summary(model))
+    predictions <- predict(model, type = "response")
+    print(predictions)
+  })
+  it("get Temporada coefficient", {
+    model <- fit_breeding_success(data = long_egg_data)
+    Temporada_coef <- get_Temporada_coef(model)
+    expect_true(Temporada_coef < 0)
   })
 })
