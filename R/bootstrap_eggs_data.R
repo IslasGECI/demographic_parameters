1bbac5cbeab06a0f6c3d861ab0e20a68cb072080
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-28 11:00:16 -0800

    ✅ Pass tests

--- R/bootstrap_eggs_data.R
+++ R/bootstrap_eggs_data.R
@@ -2,10 +2,8 @@ set_bootstrap_eggs_data <- function(raw_egg_data) {
   raw_egg_data |>
     dplyr::rowwise() |>
     dplyr::mutate(
-      egg_id = list(rep(1, No_eggs)),
-      is_chick_fledged = list(rep(1, Number_of_chicks_fledged) |>
-        c(rep(0, No_eggs - Number_of_chicks_fledged)))
+      is_chick_fledged = list(c(rep(1, Number_of_chicks_fledged), rep(0, No_eggs - Number_of_chicks_fledged)))
     ) |>
-    tidyr::unnest(c(egg_id, is_chick_fledged)) |>
-    dplyr::select(-egg_id)
+    tidyr::unnest(is_chick_fledged) |>
+    dplyr::select(Temporada, No_eggs, Number_of_chicks_fledged, is_chick_fledged)
 }
