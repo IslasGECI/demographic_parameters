17c868046759a86b6af8e2b295597693d0197f4a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-28 11:00:33 -0800

    ♻️  Refactor

--- tests/testthat/test_bootstrap_eggs_data.R
+++ tests/testthat/test_bootstrap_eggs_data.R
@@ -1,7 +1,6 @@
 describe("set egg data for bootstrapping", {
   raw_egg_data <- readr::read_csv("/workdir/tests/data/breeding_success_for_tests.csv")
   obtained <- set_bootstrap_eggs_data(raw_egg_data)
-  print(obtained$is_chick_fledged)
   it("should return a tibble with number of rows equal to the total laided egss", {
     exected_len <- sum(raw_egg_data$No_eggs)
     expect_equal(nrow(obtained), exected_len)
