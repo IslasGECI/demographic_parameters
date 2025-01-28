bcfbcf25e89319995dbefb62fa505825f6090441
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-28 10:15:18 -0800

    🛑🧪 Fail tests

--- tests/testthat/test_bootstrap_eggs_data.R
+++ tests/testthat/test_bootstrap_eggs_data.R
@@ -1,8 +1,13 @@
 describe("set egg data for bootstrapping", {
+  raw_egg_data <- readr::read_csv("/workdir/tests/data/breeding_success_for_tests.csv")
+  obtained <- set_bootstrap_eggs_data(raw_egg_data)
   it("should return a tibble with number of rows equal to the total laided egss", {
-    raw_egg_data <- readr::read_csv("/workdir/tests/data/breeding_success_for_tests.csv")
-    obtained <- set_bootstrap_eggs_data(raw_egg_data)
     exected_len <- sum(raw_egg_data$No_eggs)
     expect_equal(nrow(obtained), exected_len)
+  })
+  it("should return a tibble with number of a binary column if chicks fledged", {
+    expected_chicks_fledged <- sum(raw_egg_data$Number_of_chicks_fledged)
+    obtained_chicks_fledged <- sum(obtained$is_chick_fledged)
+    expect_equal(obtained_chicks_fledged, expected_chicks_fledged)
   })
 })
