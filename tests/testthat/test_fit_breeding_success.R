b65254a050c098215fe0c73008d43cbdd9278eb1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-28 14:43:26 -0800

    ♻️  Refactor

--- tests/testthat/test_fit_breeding_success.R
+++ tests/testthat/test_fit_breeding_success.R
@@ -3,13 +3,8 @@ describe("`fit_breeding_success()`", {
     Temporada = c(2016, 2016, 2017, 2017, 2018, 2018, 2018),
     is_chick_fledged = c(1, 0, 0, 0, 1, 1, 1)
   )
-  it("fit breeding success using binomial family", {
-    model <- fit_breeding_success(data = long_egg_data)
-    coef_values <- coef(model)
-    predictions <- predict(model, type = "response")
-  })
+  model <- fit_breeding_success(data = long_egg_data)
   it("get Temporada coefficient", {
-    model <- fit_breeding_success(data = long_egg_data)
     Temporada_coef <- get_Temporada_coef(model)
     expect_true(Temporada_coef > 0)
   })
