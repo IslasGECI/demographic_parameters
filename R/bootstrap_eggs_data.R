6b5641353ea117f2a0db8cf6b94c69373535b4b8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-29 14:09:47 -0800

    ♻️  Refactor

--- R/bootstrap_eggs_data.R
+++ R/bootstrap_eggs_data.R
@@ -36,7 +36,8 @@ Distributor <- R6::R6Class("Distribution",
   )
 )
 calculate_p_values <- function(season_parameter_distribution) {
-  p_value_decreasing <- sum(season_parameter_distribution < 0) / length(season_parameter_distribution)
-  p_value_increasing <- sum(season_parameter_distribution > 0) / length(season_parameter_distribution)
-  list("p_value_decreasing" = p_value_decreasing, "p_value_increasing" = p_value_increasing)
+  distribution_length <- length(season_parameter_distribution)
+  number_of_negatives <- sum(season_parameter_distribution < 0)
+  number_of_positives <- sum(season_parameter_distribution > 0)
+  list("p_value_decreasing" = number_of_negatives / distribution_length, "p_value_increasing" = number_of_positives / distribution_length)
 }
