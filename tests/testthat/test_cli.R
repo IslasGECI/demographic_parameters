32544ab8db286552014dcab82dd15e191d80c28a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-29 11:22:13 -0800

    🛑🧪 Fail tests

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -6,5 +6,7 @@ describe("Test cli for breeding success trend", {
     options <- list("data-path" = data_path, "B" = bootstrap_number, "output-path" = output_path)
     testtools::if_exist_remove(output_path)
     write_breeding_success_trend(options)
+
+    expect_true(testtools::exist_output_file(output_path))
   })
 })
