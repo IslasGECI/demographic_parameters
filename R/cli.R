0d3a6d64a31656c56fa735cb44e6047ad55af03e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-29 11:38:36 -0800

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -1,5 +1,8 @@
 write_breeding_success_trend <- function(options) {
-  list(B = options[["B"]]) |>
+  raw_egg_data <- readr::read_csv(options[["data-path"]], show_col_types = FALSE)
+  obtained <- get_bootstraped_season_parameter_distribution(raw_egg_data, B = options[["B"]])
+  distribution <- list("bootstrap_distribution" = obtained)
+  distribution |>
     rjson::toJSON() |>
     write(options[["output-path"]])
 }
