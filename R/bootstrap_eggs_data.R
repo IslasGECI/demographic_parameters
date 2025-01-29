2dfbc295947299d08f749a0aa6c4ba60728c53bf
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-29 10:23:53 -0800

    ♻️  Refactor

--- R/bootstrap_eggs_data.R
+++ R/bootstrap_eggs_data.R
@@ -7,10 +7,7 @@ get_bootstraped_season_parameter_distribution <- function(raw_egg_data, B) {
 set_bootstrap_eggs_data <- function(raw_egg_data) {
   raw_egg_data |>
     dplyr::rowwise() |>
-    dplyr::mutate(
-      is_chick_fledged = list(c(rep(1, Number_of_chicks_fledged), rep(0, No_eggs - Number_of_chicks_fledged)))
-    ) |>
-    tidyr::unnest(is_chick_fledged) |>
+    .add_chick_status() |>
     dplyr::select(Temporada, No_eggs, Number_of_chicks_fledged, is_chick_fledged)
 }
 
