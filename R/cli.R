af8001a6d4fb3edf5db8ab2bea99e3af5a147316
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-29 14:41:08 -0800

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -1,12 +1,23 @@
 write_breeding_success_trend <- function(options) {
   raw_egg_data <- readr::read_csv(options[["data-path"]], show_col_types = FALSE)
-  obtained <- get_bootstraped_season_parameter_distribution(raw_egg_data, B = options[["B"]]) |>
+  parameter_distribution <- get_bootstraped_season_parameter_distribution(raw_egg_data, B = options[["B"]])
+  p_values <- calculate_p_values(parameter_distribution) |>
+    rjson::toJSON()
+  parameter_distribution_string <- parameter_distribution |>
     rjson::toJSON()
   raw_egg_data_list <- raw_egg_data |>
     rjson::toJSON()
-  p_values <- calculate_p_values(obtained) |>
-    rjson::toJSON()
-  distribution <- paste0('{"raw_data":', raw_egg_data_list, ',"bootstrap_distribution":', obtained, ',"p_values":', p_values, "}")
-  distribution |>
+  json_content <- paste0('{"raw_data":', raw_egg_data_list, ',"bootstrap_distribution":', parameter_distribution_string, ',"p_values":', p_values, "}")
+  json_content |>
     write(options[["output-path"]])
+}
+
+fetch_json_content <- function(raw_egg_data, parameter_distribution) {
+  p_values <- calculate_p_values(parameter_distribution) |>
+    rjson::toJSON()
+  parameter_distribution_string <- parameter_distribution |>
+    rjson::toJSON()
+  raw_egg_data_list <- raw_egg_data |>
+    rjson::toJSON()
+  paste0('{"raw_data":', raw_egg_data_list, ',"bootstrap_distribution":', parameter_distribution_string, ',"p_values":', p_values, "}")
 }
