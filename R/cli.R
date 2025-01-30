e1afe3581230a8a6bf7372b7ce291fc0c040031e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-30 11:33:53 -0800

    ♻️  Refactor

--- R/cli.R
+++ R/cli.R
@@ -1,7 +1,8 @@
 write_breeding_success_trend <- function(options) {
   raw_egg_data <- readr::read_csv(options[["data-path"]], show_col_types = FALSE)
   parameter_distribution <- get_bootstraped_season_parameter_distribution(raw_egg_data, B = options[["B"]])
-  json_content <- fetch_json_content(raw_egg_data, parameter_distribution)
+  alpha <- 0.05
+  json_content <- xxfetch_json_content(raw_egg_data, parameter_distribution, alpha)
   json_content |>
     write(options[["output-path"]])
 }
