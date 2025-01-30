8ccb025957e75de67d35f428f18b16c3f5ed36de
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-30 11:38:06 -0800

    🛑🧪 Fail tests

--- tests/testthat/test_bootstrap_eggs_data.R
+++ tests/testthat/test_bootstrap_eggs_data.R
@@ -18,7 +18,8 @@ describe("Write JSON content", {
 describe("Give bootstrap interval", {
   it("Get interval from distribution", {
     bootstrap_distribution <- c(0:100)
-    obtained <- get_bootsrap_interval(bootstrap_distribution)
+    alpha <- 0.1
+    obtained <- xxget_bootsrap_interval(bootstrap_distribution, alpha)
     expect_equal(obtained, c(5, 50, 95))
   })
 })
